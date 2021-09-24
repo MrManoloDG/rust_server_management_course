97db3a65cfe031b844b87fd3c1a94e57a30525a5
MrManolo <manueldiazgil96@hotmail.com>
2021-09-24 20:35:46 +0200

    Concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
-use std::io::prelude::*;
-use std::fs;
-use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Hi there, booting up dude!");
-    let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
+    let handle = thread::spawn(|| {
+    	for i in 1..10 {
+        	println!("hola {} desde el primer hilo", i);
+        	thread::sleep(Duration::from_millis(10));
+    	}
+	});
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 256];
-    let get = b"GET / HTTP/1.1\r\n";
-    stream.read(&mut buffer).unwrap();
+	let handle1 = thread::spawn(|| {
+    	for i in 1..10 {
+        	println!("hola {} desde el segundo hilo", i);
+        	thread::sleep(Duration::from_millis(10));
+    	}
+	});
     
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("index.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
-        stream.write(response.as_bytes()).unwrap(); 
-        stream.flush().unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-    	let contents = fs::read_to_string("404.html").unwrap();
+	let handle2 = thread::spawn(|| {
+    	for i in 1..10 {
+        	println!("hola {} desde el tercer hilo", i);
+        	thread::sleep(Duration::from_millis(10));
+    	}
+	});
 
-    	let response = format!("{}{}", status_line, contents);
-
-    	stream.write(response.as_bytes()).unwrap();
-    	stream.flush().unwrap();
-    }
-    
-    
+    handle.join().unwrap();
+    handle1.join().unwrap();
+    handle2.join().unwrap();
 }
 
